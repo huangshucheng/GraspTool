f7fa597ed64c7927af0ecbf7005d334c44c3225a
hcc <827773271@qq.com>
2021-02-01 00:39:48 +0800

    活动修改

--- CopyData/CopyData/bin/Debug/resources/luaScript/task/TaskFoodSafe.lua
+++ CopyData/CopyData/bin/Debug/resources/luaScript/task/TaskFoodSafe.lua
@@ -9,7 +9,12 @@ local POST = TaskBase.POST
 
 TaskTMP.FIND_STRING_HOST 		= "xinhua.mofangdata.cn"
 TaskTMP.DATA_TO_FIND_ARRAY 		= {"Cookie","Referer"}
-TaskTMP.DEFAULT_KABAO_COUNT 	= 100 	-- 默认卡包次数，需要设置isKabao后才生效
+TaskTMP.DEFAULT_KABAO_COUNT 	= 150 	-- 默认卡包次数，需要设置isKabao后才生效
+
+--额外的请求头,也可以不用配置
+TaskTMP.ERQ_HEADER_EXT = {
+	["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8; application/json",
+}
 
 --任务列表
 TaskTMP.TASK_LIST_URL_CONFIG = {
@@ -21,6 +26,7 @@ TaskTMP.TASK_LIST_URL_CONFIG = {
 		urlBody = "", 
 		postBody = "id=88", 
 		delay = 0,
+		isKabao = true,
 	},
 }
 
